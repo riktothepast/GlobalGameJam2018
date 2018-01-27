69cbfd56d19359e67e063990c4da487a1d66acc3
riktothepast <rik@502studios.net>
2018-01-27 11:43:44 -0600

    gameBoard: Adds 1st phase board creation

--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -4,13 +4,47 @@ using UnityEngine;
 
 public class GameBoard : MonoBehaviour {
 
-	// Use this for initialization
+    public GameObject tilePrefab;
+    public Vector2 boardSize;
+    public int tileSize;
+    public List<Bot> bots;
+
+    enum GameStates {
+        initialization,
+        input,
+        movement,
+        boardActions,
+    }
+
+    public void CreateBaseBoard() {
+        for (int x = 0; x < boardSize.x; x += 1)
+        {
+            for (int y = 0; y < boardSize.y; y += 1)
+            {
+                GameObject tile = Instantiate(tilePrefab);
+                tile.transform.parent = transform;
+                tile.transform.position = new Vector3(x * tileSize, 0, y * tileSize);
+            }
+        }
+    }
+
+    public void AddHazards() {
+
+    }
+
+    public void InstatiateBots() {
+        Queue<Vector2> botPosition = new Queue<Vector2>();
+        botPosition.Enqueue(new Vector2(0, 0));
+        botPosition.Enqueue(new Vector2(boardSize.x, 0));
+        botPosition.Enqueue(new Vector2(0, boardSize.y));
+        botPosition.Enqueue(new Vector2(boardSize.x, boardSize.y));
+
+    }
+
 	void Start () {
-		
-	}
+        bots = new List<Bot>();
+        CreateBaseBoard();
+        InstatiateBots();
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }

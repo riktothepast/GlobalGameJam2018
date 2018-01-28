0858f4922cfc520134ea3a5c29caf456c038ebce
denn1s <dennis.aldana@gmail.com>
2018-01-27 19:46:21 -0600

    placeholder sprites

---
+++ Assets/Scripts/ImageScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ImageScript : MonoBehaviour {
+
+	public Sprite[] sprites;
+	public Sprite[] gate;
+
+	private Image image = null;
+	private bool closing = true;
+	private bool closed = true;
+
+	// Use this for initialization
+	void Start () {
+		this.image = GetComponent<Image> ();
+		this.image.sprite = sprites[0];
+	}
+
+	public void setImage(string imageName) {
+		int index;
+		switch (imageName) {
+		case "item":
+			index = 0;
+			break;
+		case "item_active":
+			index = 1;
+			break;
+		case "neutral":
+			index = 2;
+			break;
+		case "neutral_active":
+			index = 3;
+			break;
+		default:
+			index = 0;
+			break;
+		}
+	}
+
+	public void toggleGate() {
+		this.closing = true;
+	}
+
+	private void onClose() {
+		Debug.Log ("onClose()");
+	}
+
+	private void onOpen() {
+		Debug.Log ("onOpen()");
+	}
+
+	void Update () {
+		if (this.closing) {
+			int index = (int)(Time.timeSinceLevelLoad * 15);
+			index = index % gate.Length;
+			Debug.Log (index);
+			this.image.sprite = gate[!this.closed ? index : ((gate.Length-1) - index)];
+			if (index == (gate.Length-1)) {
+				this.closing = false;
+				this.closed = !this.closed;
+
+				if (this.closed) {
+					this.onClose ();
+				} else {
+					this.onOpen ();
+				}
+			}
+		}
+	}
+}
